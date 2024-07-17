65e9a1ec2a8465d867ee842a7246160d4c8ca841
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2024-07-17 17:06:04 +0300

    updated ui styling

--- src/app/ai.service.ts
+++ src/app/ai.service.ts
@@ -15,9 +15,9 @@ export class AiService {
     const emptyIndexes = this.getEmptyIndexes(newBoard);
 
     const winningCombinations = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-      [0, 4, 8], [2, 4, 6]             // Diagonals
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+      [0, 4, 8], [2, 4, 6]             
     ];
 
     if (this.checkWin(newBoard, 'X')) {
@@ -80,9 +80,9 @@ export class AiService {
 
   checkWin(board: string[], player: string): boolean {
     const winningCombinations = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-      [0, 4, 8], [2, 4, 6]             // Diagonals
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+      [0, 4, 8], [2, 4, 6]             
     ];
 
     for (const combination of winningCombinations) {
